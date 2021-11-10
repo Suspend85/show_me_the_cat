65139f7a24ebe15fcd9e5fdd42f94f9e1e02e24c
Dmitrii <melomanko@gmail.com>
2021-11-10 08:23:04 +0000

    loader completely redesigned

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
-document.body.onload = () => {
-  setTimeout( () => {
-    const preloader = document.getElementById('page-preloader')
-    if ( !preloader.classList.contains('done') )
-    {
-      preloader.classList.add('done');
-    }
-  }, 1000);
-}
-
 const API_KEY = "3be18482-503e-4a25-bb02-5eaacc70b5e4";
 const API_URL = "https://api.thecatapi.com/v1/images/search";
-const button = document.querySelector("#button");
-const catImage = document.querySelector("#cat");
+const btn = document.querySelector("#root button");
+const catImage = document.getElementById("cat");
 
-button.addEventListener("click",
-  async () => {
-    // event.preventDefault();
-    const response = await fetch(API_URL, { headers: { "x-api-key": API_KEY } });
-    const data = await response.json();
-    catImage.src = data[0].url;
-  }
-)
+// get another image func
+const getCatImage = async () => {
+  btn.classList.remove("active");
+  catImage.classList.add("hidden");
+  btn.classList.add("active");
+  const response = await fetch(API_URL, { headers: { "x-api-key": API_KEY } });
+  const data = await response.json();
+  catImage.src = data[0].url;
+};
+
+// Button click event
+button.addEventListener("click", getCatImage);
+catImage.addEventListener("load", () => {
+  btn.classList.add("active");
+  catImage.classList.remove("hidden");
+  btn.classList.remove("active");
+});
+
+getCatImage();
 
